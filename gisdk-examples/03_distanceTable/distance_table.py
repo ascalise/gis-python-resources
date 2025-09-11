ac686da141dcaf7677191438ad4271bce072fea7
ascalise <armando.scalise@gmail.com>
2025-09-10 20:37:07 -0400

    Add a python example to Ccompute a distance table

---
+++ gisdk-examples/03_distanceTable/distance_table.py
@@ -0,0 +1,62 @@
+# Computes a distance table for all points in a selection set
+
+# Before running this macro
+
+#    - Open a map of the US,
+
+#     - Make the City/Town layer the working layer
+
+#     - Create a "My Towns" Selection Set using the Selection Toolbar
+
+import caliperpy
+import tempfile
+
+def compute_distance_table(dk, opts):
+   
+    # Create and configure the routing object
+    router = dk.CreateGisdkObject("gis_ui", "Routing.Router")
+    router.Minimize = "Time"
+    router.IncludeRestStops = True
+    router.TimeBetweenRests = 5  # minutes
+    router.RestStopDuration = 20  # minutes
+    router.FuelPrice = 3.29
+    router.SingleThreaded = True
+
+    # Speed levels: 1=slow, 4=normal, 7=fast
+    router.MajorHighwaySpeedLevel = 4
+    router.SecondaryHighwaySpeedLevel = 4
+    router.LocalHighwaySpeedLevel = 4
+    router.ArterialSpeedLevel = 4
+    router.LocalRoadSpeedLevel = 4
+    
+    ret = router.DistanceTable(opts)
+    
+    # Get the name of the output table file 
+    distance_table = next((v for k, v in ret if k == "OutFileName"), None)
+    
+    dk_table = dk.CreateGisdkObject("gis_ui", "Table", {"FileName":distance_table})
+    dk_table.View()
+ 
+
+# Initialize GISDK session
+dk = caliperpy.Maptitude.connect()
+pro = dk.Getprogram()
+print(pro)
+outfile = tempfile.mktemp(suffix=".bin")
+
+# Select a few towns
+print ("Selecting Towns")
+dk.SetLayer ("City/Town")
+query_string = "Select * where name = 'Cincinnati OH'  or name = 'Colerain OH' or name = 'Amberley OH' or  name = 'Dillionvale OH'"
+dk.SelectByQuery("My Towns", "Several", query_string)
+
+# Set up distance table options
+opts = {
+        "Origins": "My Towns",
+        "Destinations": "My Towns", 
+        "IsEuclid": "False",
+        "OutputFileName": outfile
+        }
+
+table_file = compute_distance_table(dk, opts)
+caliperpy.Maptitude.disconnect()
---
+++ gisdk-examples/03_distanceTable/readme.md
@@ -0,0 +1,24 @@
+# Example: Routing
+
+This exmaple shows how to create a distance table for all points in a selection set 
+ 
+---
+
+## Inputs
+
+- None.  The script generates a selection set with 3 points
+
+---
+
+## Outputs
+
+- A distance table matrix
+
+---
+
+## How to Run
+1. Open Maptitude manually
+2. Open a new map of the Unites States  *(File > New Workspace > New map of the United States)*.  For running the scripot on a country other than the US, you would need to replace the hardwired town names accordingly.
+
+```bash
+python distance_table.py
